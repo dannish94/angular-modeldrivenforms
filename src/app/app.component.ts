000bb9dbcb23cbaf2d9191ee7bbda1f76ce86e34
Dannish Hussain <Dannishh94@gmail.com>
2017-06-07 13:49:14 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
+import { User } from './user.interface';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  moduleId: module.id,
+  selector: 'my-app',
+  templateUrl: 'app.component.html',
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+    public myForm: FormGroup;
+    public submitted: boolean;
+    public events: any[] = [];
+
+    constructor(private _fb: FormBuilder) { }
+
+    ngOnInit() {
+     
+        this.myForm = this._fb.group({
+            name: ['', [<any>Validators.required, <any>Validators.minLength(5)]],
+            address: this._fb.group({
+                street: ['', <any>Validators.required],
+                postcode: ['TF3 2DG']
+            })
+        });
+  
+        this.subcribeToFormChanges();
+        
+        (<FormControl>this.myForm.controls['name'])
+            .setValue('Dan', { onlySelf: true });
+    }
+
+    subcribeToFormChanges() {
+        const myFormStatusChanges$ = this.myForm.statusChanges;
+        const myFormValueChanges$ = this.myForm.valueChanges;
+        
+        myFormStatusChanges$.subscribe(x => this.events.push({ event: 'STATUS_CHANGED', object: x }));
+        myFormValueChanges$.subscribe(x => this.events.push({ event: 'VALUE_CHANGED', object: x }));
+    }
+
+    save(model: User, isValid: boolean) {
+        this.submitted = true;
+        console.log(model, isValid);
+    }
 }
